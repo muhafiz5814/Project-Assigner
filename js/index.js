5b80e4113141e6e03872537c33a0f2752cd87f62
Muhafiz Raza <muhafizraza1209@gmail.com>
2024-03-30 12:36:59 +0530

    Remove prevous info panel on clicking new employee

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@ const taskForce = document.querySelector('.task-force');
 const employeeCards = document.querySelectorAll('.employee');
 const { top, left } = allEmployees.getBoundingClientRect();
 
+const removePanel = () => document.querySelector(".info-panel")?.remove()
+
 const createPanel = (x, y, name) => {
   const namePanel = document.createElement("div")
   namePanel.setAttribute("class", "info-panel")
@@ -14,6 +16,7 @@ const createPanel = (x, y, name) => {
 }
 
 allEmployees.addEventListener("click", (evt) => {
+  removePanel()
   const name = evt.target.getAttribute("data-name")
   const infoPanel = createPanel(evt.clientX - left, evt.clientY - top, name)
   allEmployees.append(infoPanel)
