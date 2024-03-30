a977b9e85278411aa23b448e5d84771563b58249
Muhafiz Raza <muhafizraza1209@gmail.com>
2024-03-30 12:19:29 +0530

    Create a name panel on clicking the image and style it.

--- js/index.js
+++ js/index.js
@@ -3,4 +3,11 @@ const taskForce = document.querySelector('.task-force');
 const employeeCards = document.querySelectorAll('.employee');
 const { top, left } = allEmployees.getBoundingClientRect();
 
-
+allEmployees.addEventListener("click", (evt) => {
+  const namePanel = document.createElement("div")
+  namePanel.setAttribute("class", "info-panel")
+  namePanel.innerText = evt.target.getAttribute("data-name")
+  namePanel.style.left = `${evt.clientX - left}px`
+  namePanel.style.top = `${evt.clientY - top}px`
+  allEmployees.append(namePanel)
+})
