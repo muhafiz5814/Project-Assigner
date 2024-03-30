43f7f3273495806f42d2704318f437d50a3f1fb7
Muhafiz Raza <muhafizraza1209@gmail.com>
2024-03-30 11:43:55 +0530

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const allEmployees = document.querySelector('.all-employees');
+const taskForce = document.querySelector('.task-force');
+const employeeCards = document.querySelectorAll('.employee');
+const { top, left } = allEmployees.getBoundingClientRect();
+
+
